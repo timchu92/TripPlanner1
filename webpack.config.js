9f47b2112af1a1bb592956feebc114468fc69f8c
Chris Chin <chin.christopher@gmail.com>
2019-09-30 11:55:22 -0400

    created new files

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  output: {
+    path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
+    filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
+  }
+}
